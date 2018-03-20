324316a55caeaa684d1daf7d9eba650bf7319c43
Claudio Scheer <claudioscheer@protonmail.com>
2018-03-20 00:51:22 -0300

    Python 3

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dependencies
+- Python 3
+- `pip3 install --upgrade youtube-dl`
+- [http://builds.libav.org/windows/release-gpl/](http://builds.libav.org/windows/release-gpl/) - to use mp3
+
+# How to run?
+- `python3 youtube-mp3.py` or `python3 youtube-video.py`
+- type url youtube video
--- youtube-video.py
+++ youtube-video.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+import sys
 import youtube_dl
 
 
-class MyLogger(object):
+class MyLogger():
     def debug(self, msg):
         pass
 
@@ -9,20 +11,23 @@ class MyLogger(object):
         pass
 
     def error(self, msg):
-        print msg
+        print(msg)
+
 
 def my_hook(d):
     if d["status"] == "finished":
-        print "Converting file..."
+        print("Converting file...")
     elif d["status"] == "downloading":
-        print "%s of %s..." % (d["_percent_str"], d["_total_bytes_str"])
+        sys.stdout.write("\r%s of %s..." % (d["_percent_str"], d["_total_bytes_str"]))
+        sys.stdout.flush()
 
 ydl_opts = {
     "format": "bestvideo+bestaudio",
     "logger": MyLogger(),
     "progress_hooks": [my_hook],
+    "noplaylist" : True,
 }
 
-print "Type YouTube url:"
+print("Type YouTube url:")
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([raw_input()])
+    ydl.download([input()])
