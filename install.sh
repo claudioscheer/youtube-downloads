6cd6611ff01c9fb769b0776693fdd5602f8d4d93
Claudio Scheer <claudioscheer@protonmail.com>
2020-06-22 17:24:37 -0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+pip install -r requirements.txt
+sudo ln -s ~/youtube-downloads/youtube-music.sh /usr/local/bin/youtube-music
+sudo ln -s ~/youtube-downloads/youtube-video.sh /usr/local/bin/youtube-video
